45ed5030093bd4a1a7fe3ff218fdc3581f157c22
Amruth Kiran <amruthkiran47@gmail.com>
2022-10-06 11:50:28 +0530

    test

--- .setup.php
+++ .setup.php
@@ -3,7 +3,7 @@ include("modules/system.php");
 class set {
   public function Setup() {
     global $system;
-    // removing old files
+    
     if ($system=="termux") {
       system("rm -rf /data/data/com.termux/files/usr/share/IP-Tracer");
       system("rm -rf /data/data/com.termux/files/usr/bin/ip-tracer");
@@ -18,7 +18,7 @@ class set {
       system("rm -rf /usr/share/IP-Tracer");
     }
     
-    // adding bin file
+
     if ($system=="termux") {
       system("mv -v modules/ip-tracer /data/data/com.termux/files/usr/bin/");
       system("mv -v modules/trace /data/data/com.termux/files/usr/bin/");
@@ -36,7 +36,7 @@ class set {
       system("chmod +x /usr/bin/trace");
     }
 
-    // copy files from IP-Tracer to .IP-Tracer directory.
+
     if ($system=="termux") {
       system("mkdir /data/data/com.termux/files/usr/share/IP-Tracer");
       system("chmod +x * *.* .*.*");
@@ -51,7 +51,6 @@ class set {
       system("mv -v * *.* .*.* /usr/share/IP-Tracer/");
     }
     
-    // removing IP-Tracer directory
     if ($system=="termux") {
       system("cd .. && rm -rf IP-Tracer");
     } elseif ($system=="ubuntu") {
@@ -72,27 +71,15 @@ class set {
      | |  __/  | |_| | | (_| | (_|  __/ |
      |_|_|      \__|_|  \__,_|\___\___|_|
 
-    a
-    \033[01;37m}\033[01;31m--------------------------------------\033[01;37m{
- }\033[01;31m------------- \033[01;32mTrack IPLocation\033[01;31m -------------\033[01;37m{
-    }\033[01;31m--------------------------------------\033[01;37m{
+    Raghav Jindal , Amruth Kiran
 
-\033[00m
 EOL;
 
     if (file_exists("/usr/bin/ip-tracer") || file_exists("/data/data/com.termux/files/usr/bin/ip-tracer")) {
-      echo "\033[01;32m      IP-Tracer installed Successfully !!!\033[00m\n";
+      echo "\033[01;32m     \033[00m\n";
       echo <<<EOL
 
-\033[01;37m ----------------------------------------------
-|         \033[01;36mcommand\033[01;37m       |        \033[01;36mUse\033[01;37m           |
- ----------------------------------------------
-| \033[01;32mtrace -m\033[01;37m              | \033[01;33mTrack your IP\033[01;37m        |
-| \033[01;32mtrace -t <traget-ip>\033[01;37m  | \033[01;33mTrack IP\033[01;37m             |
-| \033[01;32mtracer --help\033[01;37m         | \033[01;33mFor more information\033[01;37m |
- ----------------------------------------------
 
-\033[01;31mNote :- ip-api will automatically ban any IP addresses doing over 150 requests per minute.\033[00m
 
 
 EOL;
